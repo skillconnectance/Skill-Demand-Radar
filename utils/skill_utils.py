a5b697379c2244f3a06a9b7b041ec8ffd0602b5b
skillconnectance <sc.teamcenter@gmail.com>
2025-05-12 18:09:06 +0530

    Update skill_utils.py

--- utils/skill_utils.py
+++ utils/skill_utils.py
@@ -1,9 +1,8 @@
 import pandas as pd
 
-def load_skill_data(filepath="data/all_data_skill_and_non_skill.csv"):
-    df = pd.read_csv(filepath)
-    df.columns = [col.strip().lower().replace(" ", "_") for col in df.columns]
-    return df
+def load_skill_data(filepath="data/all_data_skill_and_nonskills.csv"):
+    import pandas as pd
+    return pd.read_csv(filepath)
 
 def match_user_skills(user_skills, skill_df):
     user_skills = [skill.lower().strip() for skill in user_skills]
